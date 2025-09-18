6a5751c918626b3dbdb674dc4e80857c96e7eb14
wardr19 <wardr19@gmatc.matc.edu>
2025-09-18 18:00:23 -0500

    activity-2 fixes for mongoDb

--- config/Db.js
+++ config/Db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import config from 'config';    
 
 //get the connection string
-const db = config.get('mongoURI');
+const db = config.get('mongoURL');
 
 //function to connect to MongoDB
 const connectDataBase = async () => {
